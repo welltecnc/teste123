2924025f362dbc2b0877640ed95c616a5e45a190
Wellington <welltecnic@hotmail.com>
2024-10-16 15:02:40 -0300

    teste4

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -15,8 +15,8 @@ const Home = () => {
                     <form>
                         <div className="logos">
                             <img className="fe" src={FormulaE1} alt="Formula1" />
-                            <img className="dot" src={ponto} alt="" />
-                            <img className="mahindra" src={mahindra} alt="" />
+                            <img className="dot" src={ponto} alt="teste" />
+                            <img className="mahindra" src={mahindra} alt="teste" />
                         </div>
                         <p>A Tech Mahindra em parceria com a Formula E colaboraram para aprimorar o desenvolvimento de soluções tecnológicas e digitais nas experiências da Formula E no Brasil. As mesmas colaboraram com a Data Sphere Solutions para expandir a popularidade da categoria no país.</p>
                     </form>
@@ -27,7 +27,7 @@ const Home = () => {
                 <div class="container2">
                     <form className="form2">
                         <h2>O que é a Data Sphere?</h2>
-                        <img src={data} alt="" />
+                        <img src={data} alt="teste" />
                         <p>Data Sphere é uma empresa fictícia criada para simular uma equipe do mercado representando ao nosso grupo para as entregas dos trabalhos. Nossa solução para este problema foi a criação de atividades escolares. Para mais informações, veja a página de <a href="/atividades">Atividades</a>. </p>
                     </form>
                 </div>
